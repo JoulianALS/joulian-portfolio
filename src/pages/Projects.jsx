96571a404a54b33d60202a83d6630c99fbb901aa
JoulianALS <joulianalsuliman@gmail.com>
2025-04-01 08:30:30 +0200

    Fix attempt 2

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -63,7 +63,7 @@ const sections = [
     title: "Spiele",
     images: [
       {
-        src: "public/OutOfLight.png",
+        src: "../../../public/OutOfLight.png",
         title: "OutOfLight",
         description: "Ein kurzes First-Person Horrorspiel in Unity.",
         technologies: "Unity, C#",
