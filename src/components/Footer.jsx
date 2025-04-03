f2825749704387573e386d7ecded61c9ea068acd
JoulianALS <joulianalsuliman@gmail.com>
2025-04-03 12:49:47 +0200

    LinkedIn removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,23 +7,14 @@ function Footer() {
       {/* Copyright Text */}
       <p>&copy; Joulian Alsuliman 2025. Alle Rechte vorbehalten.</p>
 
-      {/* Social Links */}
-      <div className="social-links">
-        <a
-          href="https://github.com/JoulianALS"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub size={30} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/joulian-alsuliman-06004535a/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedin size={30} />
-        </a>
-      </div>
+      {/* GitHub Link */}
+      <a
+        href="https://github.com/JoulianALS"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub size={30}/>
+      </a>
     </footer>
   );
 }
