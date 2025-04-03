e8564408e2f82ed5d394b4a1f1e429805c488bc3
JoulianALS <joulianalsuliman@gmail.com>
2025-04-03 12:42:16 +0200

    Added linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,23 @@ function Footer() {
       {/* Copyright Text */}
       <p>&copy; Joulian Alsuliman 2025. Alle Rechte vorbehalten.</p>
 
-      {/* GitHub Link */}
-      <a
-        href="https://github.com/JoulianALS"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaGithub size={30}/>
-      </a>
+      {/* Social Links */}
+      <div className="social-links">
+        <a
+          href="https://github.com/JoulianALS"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={30} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/joulian-alsuliman-06004535a"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin size={30} />
+        </a>
+      </div>
     </footer>
   );
 }
